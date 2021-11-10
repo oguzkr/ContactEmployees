a72e059ecf752f2103d33c9b5bd38eb5b93abac7
Oğuz <oguzkaratoruk@yandex.com>
2021-11-11 00:03:06 +0300

    less buggy

--- ContactEmployees/View/ContactDetailVC.swift
+++ ContactEmployees/View/ContactDetailVC.swift
@@ -19,44 +19,46 @@ class ContactDetailVC: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
         bindEmployeeData()
-        tableView.register(UINib(nibName: ProjectCell.reuseID, bundle: nil), forCellReuseIdentifier: Identifiers.projectCell)
     }
     
     func bindEmployeeData(){
         if let selectedEmployee = employee {
             labelFullName.text = "\(selectedEmployee.fname) \(selectedEmployee.lname)"
+            
             labelPosition.text = selectedEmployee.position
+            
             selectedEmployee.contact_details?.phone == nil ? (labelPhone.isHidden = true) : (labelPhone.text = selectedEmployee.contact_details?.phone)
+            
             labelEmail.text = selectedEmployee.contact_details?.email
         }
     }
+    
+    func configureTableView(){
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: Identifiers.projectCell)
+    }
 
+    //MARK: TablView Methods
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.section == 3 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.projectCell, for: indexPath) as! ProjectCell
-
+            let cell = tableView.dequeueReusableCell(withIdentifier: Identifiers.projectCell)!
             if let employeeProjects = employee?.projects {
                 if employeeProjects.count > 0 {
-                    cell.labelProject.text = employeeProjects[indexPath.row]
+                    cell.textLabel?.text = employeeProjects[indexPath.row]
                 }
             }
         }
         return super.tableView(tableView, cellForRowAt: indexPath)
-    
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        if section == 3  {
-            if employee?.projects == nil {
-                return ""
-            }
+        if section == 3 && employee?.projects == nil{
+            return ""
         }
         return super.tableView(tableView, titleForHeaderInSection: section)
     }
     
-    
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let employeeProjects = employee?.projects {
             if section == 3 {
